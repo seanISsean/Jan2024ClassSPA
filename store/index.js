eac3f710f699cfef7f5319fc619684b0228f7355
Sean Okpala <seane8088@gmail.com>
2024-03-12 21:24:48 -0500

    so class spa

--- store/index.js
+++ store/index.js
@@ -3,4 +3,4 @@ export { default as Home } from "./Home";
 export { default as Order } from "./Order";
 export { default as Pizza } from "./Pizza";
 export { default as Viewnotfound } from "./Viewnotfound";
-export { default as Links } from "./Links";
+export { default as Menu } from "./Menu";
